3ecdeb4071bc2866bb29cba5d4fbeada73580f33
Patty8909 <patty90_30@hotmail.com>
2018-03-14 09:50:05 -0500

    componentes

--- src/components/app.js
+++ src/components/app.js
@@ -1,9 +1,53 @@
 import React, { Component } from 'react';
 
-export default class App extends Component {
-  render() {
-    return (
-      <div>React simple starter</div>
-    );
-  }
+const App = () => (
+  <div>
+    <Header />
+    <Section />
+    <ListaHobbies />
+  </div>
+)
+
+const Header = () => {
+  return (
+    <div>
+      <h1>About Me</h1>
+    </div>
+  )
 }
+
+const Section = () => {
+  return (
+    <div>
+      <section>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec in viverra felis. Proin pulvinar nulla erat, at blandit sem rutrum id. Sed laoreet, est varius laoreet pulvinar, diam odio sollicitudin massa, quis posuere leo nulla id magna. Sed diam lectus, lacinia vel massa condimentum, venenatis maximus purus.</p>
+      </section>
+    </div>
+  )
+}
+
+const hobbies = [
+  { id: 1, text: 'Leer'},
+  { id: 2, text: 'Ver películas'},
+  { id: 3, text: 'Aprender manualidades'},
+  { id: 4, text: 'Cocinar'},
+]
+
+const ListaHobbies = () => (
+  <ul>
+    {hobbies.map(hobbie =>
+      <Todo
+        key={hobbie.id}
+        {...hobbie}
+      />
+    )}
+  </ul>
+)
+
+render(
+  <App />,
+  document.getElementById('root')
+)
+
+
+
